8afd3f4493057cca70d6b15014c113be7f9a4026
Elena  K <soldatenko_elena@mail.ru>
2012-10-01 21:05:27 +0700

    Unitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c4df2447e2efa29161a35cacc1be04d048fa8640698c902638958858b377237bcf7a76679d83a4f0c14b1b4f9ba656cdc760d7243f90d4bd62141d0a91e13e29'
